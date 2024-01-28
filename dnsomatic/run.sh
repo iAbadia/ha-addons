978f40bdda5d88a3831e1a8953c4e59113339fa4
Inaki Abadia <i.abadiaosta@gmail.com>
2024-01-28 09:04:22 +0000

    Another one

--- dnsomatic/run.sh
+++ dnsomatic/run.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Hello world!"
+declare domain
+declare email
+declare api_token
+declare update_interval
+
+domain=$(bashio::config 'domain')
+email=$(bashio::config 'email')
+api_token=$(bashio::config 'api_token')
+update_interval=$(bashio::config 'update_interval')
+
+echo "${domain}"
+echo "${email}"
+echo "${api_token}"
+echo "${update_interval}"
